7e0cd3b06e66d728648c2b8e522a8a4937bd16e0
Blaze <140644420+mkhalidh@users.noreply.github.com>
2024-10-30 22:10:27 +0500

    commit

---
+++ Backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4001;  // Default to 4001 if not specified
+const URI = process.env.MongoDBAtlasURI;
+
+// Connect to MongoDB
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Connected to MongoDB Atlas");
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB Atlas: ", error.message);
+});
+
+console.log(URI)
+// Defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
